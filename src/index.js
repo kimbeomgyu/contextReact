304f29313dc7c9b63212f2591d04086eba3c7dee
kimbeomgyu <qjarb1994@gmail.com>
2020-04-27 23:04:00 +0900

    Creating the Store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "Components/App";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import "./typography";
+
+createGlobalStyle`
+${reset};
+body {
+  background-color:#ecf0f1;
+}`;
 
 ReactDOM.render(
   <React.StrictMode>
